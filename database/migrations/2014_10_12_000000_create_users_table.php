10ebee9151810d2c28f3cdbb08b60666a1d182c2
Thang Dep Zai <winnerbui0803@gmail.com>
2023-09-28 16:34:00 +0700

    thêm trường trường avata

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('phone_number');
+            $table->numericMorphs('phone_number');
+            $table->string('avata')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
